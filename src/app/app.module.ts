9bbdd6180a9ec46710d54f6c9940c3ab223ec6a8
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-09-12 17:21:49 +0800

    Update app imports and app test file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { SignupComponent } from './signup/signup.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: SignupComponent },
+      { path: 'checkout', component: CheckoutComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    TopBarComponent,
+    ProductListComponent,
+    CartComponent,
+    SignupComponent,
+    CheckoutComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
