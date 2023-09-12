090188b31fbbba1919e73ac3c061fdead16a988c
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-09-12 17:25:47 +0800

    Related files for cart component and service

---
+++ src/app/cart/cart.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import { Product } from '../product/products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+
+  constructor() {}
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getProductCount() {
+    return this.items.length;
+  }
+
+  deleteItem(product: Product) {
+    const index: number = this.items.indexOf(product);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
