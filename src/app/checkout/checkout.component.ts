1c33058bd8fe1014a7d94b5f728f1e6d0b63a02d
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-09-12 17:26:20 +0800

    Checkout component

---
+++ src/app/checkout/checkout.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CartService } from '../cart/cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css'],
+})
+export class CheckoutComponent {
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {}
+  items = this.cartService.getItems();
+  cartCount = this.cartService.getProductCount();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+    contact_number: '',
+  });
+
+  onCheckout(): void {
+    // Process checkout data here
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+}
