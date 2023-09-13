07df00713f9ca6b0002c16b76ba062f92df5d8e2
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-09-14 07:43:53 +0800

    Product details component

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product/product.service';
+import { Observable } from 'rxjs';
+import { Product } from '../product/products';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product!: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+  ) {}
+
+  ngOnInit(): void {
+    // Get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productId = Number(routeParams.get('productId'));
+
+    this.productService.getProductById(productId).subscribe((data: Product) => {
+      this.product = data;
+    });
+  }
+}
