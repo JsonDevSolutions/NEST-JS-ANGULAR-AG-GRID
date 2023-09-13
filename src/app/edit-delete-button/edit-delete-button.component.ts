63ded1071752439a7552cd6755fb564c8d7c84b5
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-09-14 07:44:17 +0800

    Custom edit and delete component

---
+++ src/app/edit-delete-button/edit-delete-button.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { ICellRendererParams } from 'ag-grid-community';
+import { ProductService } from '../product/product.service';
+
+@Component({
+  selector: 'app-edit-delete-button',
+  templateUrl: './edit-delete-button.component.html',
+  styleUrls: ['./edit-delete-button.component.css'],
+})
+export class EditDeleteButtonComponent implements ICellRendererAngularComp {
+  productId!: number;
+
+  constructor(private productService: ProductService) {}
+
+  refresh(params: ICellRendererParams<any, any, any>): boolean {
+    return false;
+  }
+  agInit(params: ICellRendererParams): void {
+    this.productId = params.data.id;
+  }
+
+  onDelete() {
+    const deleteProduct = this.productService
+      .deleteProduct(this.productId)
+      .subscribe();
+  }
+}
