79c5d697859bf3c06366f175e0ca54e15fa4f8b0
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-09-17 09:24:25 +0800

    Refresh cart items after successful login

--- Angular Front End/src/app/pages/product-list/product-list.component.ts
+++ Angular Front End/src/app/pages/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 import { Product } from '../../core/interfaces/product.interface';
 import { ProductService } from '../product/product.service';
+import { CartService } from '../cart/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,11 +11,15 @@ import { ProductService } from '../product/product.service';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+  ) {}
 
   products!: Observable<Product[]>;
 
   ngOnInit(): void {
+    this.cartService.getCartItems().subscribe();
     this.products = this.productService.getProducts();
   }
 }
